5d0b236dfe8f429632ce6b8be9def844abf7458c
daaleksandrov <daaleksandrov@domclick.ru>
2020-08-18 07:32:23 +0300

    Setup project:
    - Add PWA support
    - Add Bootstrap
    - Add Firebase
    - Add Routing

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,17 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase: {
+    apiKey: 'AIzaSyBSIEmhV8Kp1fDSzDlAm7FJwjlog5BXHhw',
+    authDomain: 'shop-c9232.firebaseapp.com',
+    databaseURL: 'https://shop-c9232.firebaseio.com',
+    projectId: 'shop-c9232',
+    storageBucket: 'shop-c9232.appspot.com',
+    messagingSenderId: '751312431168',
+    appId: '1:751312431168:web:a5569887b0c6b166d497a5',
+    measurementId: 'G-1D3PYP1ZZQ'
+  }
 };
 
 /*
